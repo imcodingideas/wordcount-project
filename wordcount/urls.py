d988e1a5e72b472143380e4996e58900a98de36f
Joseph Chambers <joseph@michael-chambers.com>
2018-03-24 13:52:48 -0700

    count template

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('count/', views.count, name='count'),
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-  return render(request, 'home.html', {'Hithere':'This is a dict'})
+  return render(request, 'home.html')
+
+def count(request):
+  return render(request, 'count.html')
